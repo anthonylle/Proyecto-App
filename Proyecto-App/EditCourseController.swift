336c91d61c29f81cc9fc8f64b8f0f49feec4e3d9
Anthony Leandro <aleandro@git.ec.tec.ac.cr>
2019-05-14 07:47:37 -0600

    CRUD Operations completely working at Views

---
+++ Proyecto-App/EditCourseController.swift
@@ -0,0 +1,30 @@
+//
+//  EditCourseController.swift
+//  Proyecto-App
+//
+//  Created by Anthony Leandro on 5/14/19.
+//  Copyright © 2019 Anthony Leandro. All rights reserved.
+//
+
+import UIKit
+
+class EditCourseController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
